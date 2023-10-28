83df6c23f68dc4cae6a06eaeaccdeb31ac30f310
berkfatihturan <bft_@outlook.com>
2023-10-28 19:23:12 +0300

    Enhancement: Improved code organization and functionality
    Changes:
    - Updated 'message_records.json' file added.
    - updated msg_code

--- src/WebScraper.py
+++ src/WebScraper.py
@@ -32,10 +32,10 @@ class WebScraper:
 
         # set chrome driver settings
         chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--disable-gpu")
-        # chrome_options.add_argument("--no-sandbox")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
         # open browser and go to page
