c2cf635e33fd6e715bb29a573722f793b7d7cbd9
Nihat Çetin <96945187+nihatctnn@users.noreply.github.com>
2023-10-31 14:22:29 +0300

    Update WebScraper.py

--- src/WebScraper.py
+++ src/WebScraper.py
@@ -25,10 +25,12 @@ class WebScraper:
 
         # set chrome driver settings
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.addArguments("start-maximized"); // open Browser in maximized mode
+        chrome_options.addArguments("disable-infobars"); // disabling infobars
+        chrome_options.addArguments("--disable-extensions"); // disabling extensions
+        chrome_options.addArguments("--disable-gpu"); // applicable to windows os only
+        chrome_options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        chrome_options.addArguments("--no-sandbox"); // Bypass OS security model
         self.driver = webdriver.Chrome(service=service, options=chrome_options)
 
         # open browser and go to page
