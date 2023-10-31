9e0c19da1ae947a51edfc57432ee0d96e67e224e
Nihat Çetin <96945187+nihatctnn@users.noreply.github.com>
2023-10-31 14:46:34 +0300

    Update DatabaseHandler.py

--- src/DatabaseHandler.py
+++ src/DatabaseHandler.py
@@ -4,12 +4,11 @@ import config
 
 class DatabaseHandler:
     def __int__(self):
-        pass
+        self.client = pymongo.MongoClient(config.DB_URI)
+        self.mydb = self.client[config.DB_NAME]
         
 
     def add_data(self, data):
-        self.client = pymongo.MongoClient(config.DB_URI)
-        self.mydb = self.client[config.DB_NAME]
         self.mycoll = self.mydb[config.DB_COLLECTION]
         self.mycoll.insert_one(data)
         print("Veri ekleme tamamlandı.")
